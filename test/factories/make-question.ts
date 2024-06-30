52c5cd032da50ecb49db344a1b610055ae7e0559
bzenky <bzenky@gmail.com>
2024-06-30 19:30:55 -0300

    using fake data with faker

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
@@ -5,14 +6,20 @@ import {
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Default Title',
-    content: 'Lorem ipsum dolor sit amet, consectetur adip',
-    slug: Slug.create('Default Slug'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      slug: Slug.create('Default Slug'),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
