d649c0f625f84e6952ce7ee0c4b887a7dbf0949c
bzenky <bzenky@gmail.com>
2024-08-10 16:29:33 -0300

    use case: fetch answer comments

--- src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -6,14 +6,14 @@ import { makeQuestionComment } from "test/factories/make-question-comment";
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
 let sut: FetchQuestionCommentsUseCase;
 
-describe("Fetch Questions Comments", () => {
+describe("Fetch Question Comments", () => {
   beforeEach(() => {
     inMemoryQuestionCommentsRepository =
       new InMemoryQuestionCommentsRepository();
     sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository);
   });
 
-  it("should be able to fetch questions comments", async () => {
+  it("should be able to fetch question comments", async () => {
     await inMemoryQuestionCommentsRepository.create(
       makeQuestionComment({
         questionId: new UniqueEntityID("question-01"),
@@ -40,7 +40,7 @@ describe("Fetch Questions Comments", () => {
     expect(questionComments).toHaveLength(3);
   });
 
-  it("should be able to fetch paginated questions comments", async () => {
+  it("should be able to fetch paginated question comments", async () => {
     for (let i = 1; i <= 22; i++) {
       await inMemoryQuestionCommentsRepository.create(
         makeQuestionComment({ questionId: new UniqueEntityID("question-01") })
