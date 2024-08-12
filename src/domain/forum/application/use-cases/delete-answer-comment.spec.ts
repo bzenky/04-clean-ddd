390e6b20a9e5fdd0b521613593ce9d07fe313418
bzenky <bzenky@gmail.com>
2024-08-11 21:36:59 -0300

    generic errors

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
 import { DeleteAnswerCommentUseCase } from "./delete-answer-comment";
 import { makeAnswerComment } from "test/factories/make-answer-comment";
+import { NotAllowedError } from "./errors/not-allowed-error";
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
 let sut: DeleteAnswerCommentUseCase;
@@ -17,12 +18,13 @@ describe("Delete Answer Comment", () => {
 
     await inMemoryAnswerCommentsRepository.create(answerComment);
 
-    await sut.execute({
+    const result = await sut.execute({
       answerCommentId: answerComment.id.toString(),
       authorId: answerComment.authorId.toString(),
     });
 
     expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
+    expect(result.isRight()).toBe(true);
   });
 
   it("should not be able to delete comment on answer", async () => {
@@ -30,11 +32,12 @@ describe("Delete Answer Comment", () => {
 
     await inMemoryAnswerCommentsRepository.create(answerComment);
 
-    expect(() => {
-      return sut.execute({
-        answerCommentId: answerComment.id.toString(),
-        authorId: "xablau",
-      });
-    }).rejects.toBeInstanceOf(Error);
+    const result = await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: "xablau",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
