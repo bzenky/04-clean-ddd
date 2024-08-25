eb0bd1a4c51947ef6cc8123f416b30aec6a0ebb7
bzenky <bzenky@gmail.com>
2024-08-25 20:04:48 -0300

    fix edit question files

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -3,14 +3,22 @@ import { EditQuestionUseCase } from "./edit-question";
 import { makeQuestion } from "test/factories/make-question";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
 let sut: EditQuestionUseCase;
 
 describe("Edit Question", () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
-    sut = new EditQuestionUseCase(inMemoryQuestionsRepository);
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository();
+    sut = new EditQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionAttachmentsRepository
+    );
   });
 
   it("should be able to edit own question", async () => {
@@ -23,17 +31,41 @@ describe("Edit Question", () => {
 
     await inMemoryQuestionsRepository.create(newQuestion);
 
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID("1"),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID("2"),
+      })
+    );
+
     const result = await sut.execute({
       authorId: "author-01",
-      questionId: newQuestion.id.toString(),
+      questionId: newQuestion.id.toValue(),
       title: "Updated Title",
       content: "Updated Content",
+      attachmentsIds: ["1", "3"],
     });
 
     expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
       title: "Updated Title",
       content: "Updated Content",
     });
+
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems
+    ).toHaveLength(2);
+
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID("3") }),
+    ]);
+
     expect(result.isRight()).toBe(true);
   });
 
@@ -49,9 +81,10 @@ describe("Edit Question", () => {
 
     const result = await sut.execute({
       authorId: "author-01",
-      questionId: "question-01",
+      questionId: newQuestion.id.toValue(),
       title: "Updated Title",
       content: "Updated Content",
+      attachmentsIds: [],
     });
 
     expect(result.isLeft()).toBe(true);
--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -3,6 +3,10 @@ import { QuestionsRepository } from "../repositories/questions-repository";
 import { QuestionAttachmentList } from "@/domain/forum/enterprise/entities/question-attachment-list";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
+import { Either, left, right } from "@/core/either";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { NotAllowedError } from "./errors/not-allowed-error";
+import { Question } from "../../enterprise/entities/question";
 
 interface EditQuestionUseCaseRequest {
   authorId: string;
@@ -12,7 +16,12 @@ interface EditQuestionUseCaseRequest {
   attachmentsIds: string[];
 }
 
-interface EditQuestionUseCaseResponse {}
+type EditQuestionUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    question: Question;
+  }
+>;
 
 export class EditQuestionUseCase {
   constructor(
@@ -30,11 +39,11 @@ export class EditQuestionUseCase {
     const question = await this.questionsRepository.findById(questionId);
 
     if (!question) {
-      throw new Error("Question not found.");
+      return left(new ResourceNotFoundError());
     }
 
     if (question.authorId.toString() !== authorId) {
-      throw new Error("Unauthorized to edit this question.");
+      return left(new NotAllowedError());
     }
 
     const currentQuestionsAttachments =
@@ -59,6 +68,8 @@ export class EditQuestionUseCase {
 
     await this.questionsRepository.save(question);
 
-    return {};
+    return right({
+      question,
+    });
   }
 }
