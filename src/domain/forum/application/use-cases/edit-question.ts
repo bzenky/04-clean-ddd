5458779b0dd95569541c942cfcf996726b75c722
bzenky <bzenky@gmail.com>
2024-08-25 16:17:47 -0300

    fix edit-question file

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,49 +1,64 @@
-import { Either, left, right } from "@/core/either";
-import { Question } from "../../enterprise/entities/question";
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionsRepository } from "../repositories/questions-repository";
-import { ResourceNotFoundError } from "./errors/resource-not-found-error";
-import { NotAllowedError } from "./errors/not-allowed-error";
+import { QuestionAttachmentList } from "@/domain/forum/enterprise/entities/question-attachment-list";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
 
 interface EditQuestionUseCaseRequest {
   authorId: string;
   questionId: string;
   title: string;
   content: string;
+  attachmentsIds: string[];
 }
 
-type EditQuestionUseCaseResponse = Either<
-  ResourceNotFoundError | NotAllowedError,
-  {
-    question: Question;
-  }
->;
+interface EditQuestionUseCaseResponse {}
 
 export class EditQuestionUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionAttachmentsRepository: QuestionAttachmentsRepository
+  ) {}
 
   async execute({
     authorId,
     questionId,
     title,
     content,
+    attachmentsIds,
   }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId);
 
     if (!question) {
-      return left(new ResourceNotFoundError());
+      throw new Error("Question not found.");
     }
 
     if (question.authorId.toString() !== authorId) {
-      return left(new NotAllowedError());
+      throw new Error("Unauthorized to edit this question.");
     }
+
+    const currentQuestionsAttachments =
+      await this.questionAttachmentsRepository.findManyByQuestionId(questionId);
+
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionsAttachments
+    );
+
+    const questionAttachments = attachmentsIds.map((id) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(id),
+        questionId: question.id,
+      });
+    });
+
+    questionAttachmentList.update(questionAttachments);
 
     question.title = title;
     question.content = content;
+    question.attachments = questionAttachmentList;
 
     await this.questionsRepository.save(question);
 
-    return right({
-      question,
-    });
+    return {};
   }
 }
